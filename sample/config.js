797e2aade1d8c252541b1e58a5da65eed8c67f90
Ramesh Sampath <ramesh@sampathweb.com>
2013-03-05 16:46:23 -0600

    cofig check

--- sample/config.js
+++ sample/config.js
@@ -5,9 +5,9 @@ config.app = {};
 config.app.port = 8080;
 config.api = {};
 
-config.api.base_url = 'https://api.sandbox.inbloom.org';
-config.api.client_id = 'fUkLaEnwOr'
-config.api.client_secret = 'aVN7wa00MoPgNRLT2EHeDp2t0vqfyO2foxRczxLJzowheNI3';
+config.api.base_url = 'https://api.sandbox.slcedu.org';
+config.api.client_id = 'hL1CgBYFFW'
+config.api.client_secret = 'Oj9uib4gpa1LEP38wG6Imyd6ljeiKuEvoonCwusJF2skBevQ';
 config.api.oauth_uri = 'http://127.0.0.1:8080/oauth';
 config.api.api_version = "v1.1";
 
